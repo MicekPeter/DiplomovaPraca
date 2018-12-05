ee1afc3558df150cf10f3380323191a2496fcd5e
Banan <Banan@147.232.152.162>
2018-12-05 16:42:17 +0100

    initial commit

---
+++ DiplomovaPraca/src/diplomovapraca/DiplomovaPraca.java
@@ -0,0 +1,172 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diplomovapraca;
+
+/**
+ *
+ * @author Banan
+ */
+public class DiplomovaPraca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //inicializacia
+        //nastavitelne premenne
+        int max_w = 2; //maximalna vaha predmetu (weight)
+        int min_w = 1; //minimalna vaha predmetu (weight)
+        int max_p = 40; //maximalne bodove ohodnotenie (points)
+        int min_p = 1; //minimalne bodove ohodnotenie (points)
+        int max_b = 50; //objem vedra (bucket)
+        int k = 10; //pocet predmetov
+        int p = 500; // pocet iteracii (zacina od 0)
+        int rmut = 100; //rozsah pre generovanie mutacie (interval 0 - rmut)
+        int prah = 60; //prah mutacie
+        int gen = 12; //pocet vygenerovani vektorov naplnenia (musi byt delit 4)
+        int kriz = 1; //kolko bodove krizenie ma prebiehat ('1' = jednobodove krizenie | '2' = dvojbodove krizenie)
+
+        //int
+        int best_points = 0;
+        int best_weight = 0;
+        int best_error = k * max_w;
+
+        //int[]
+        int[] bucket_weight = new int[gen];
+        int[] bucket_points = new int[gen];
+        int[] temp_arr = new int[gen];
+        int[] weights = new int[k];
+        int[] points = new int[k];
+        int[] bucket = new int[k];
+        int[] error = new int[gen];
+        int[] best_solution = new int[k];
+        int[] pamat = new int[gen / 2];
+
+        //String[]
+        String[] spojeny_solutions = new String[(gen / 2)];
+        String[] final_solutions = new String[gen];
+        String[] final_solutions_strings = new String[gen];
+
+        //int[][]
+        int[][] solutions = new int[gen][k];
+        int[][] solutions_50 = new int[gen / 2][k];
+
+        //Zaciatok programu
+        for (int t = 0; t < p; t++) {
+            System.out.println("Iteracia: " + t + " ------------------------------------------------------------------------------------------------");
+
+            if (t == 0) {
+                //generovanie prvkov
+                Funkcie.GenerateItems(k, max_w, min_w, max_p, min_p, weights, points);
+
+                //generovanie vedier
+                Funkcie.GenerateBuckets(gen, k, bucket, bucket_weight, weights, bucket_points, points, solutions);
+
+                //pocitanie chyby
+                Funkcie.ErrorCount(gen, max_b, error, bucket_weight);
+
+                //triedenie
+                Funkcie.Sort(gen, error, temp_arr, bucket_weight, bucket_points, solutions);
+
+                //vyber najlepsieho
+                System.arraycopy(solutions[0], 0, best_solution, 0, k);
+                best_error = error[0];
+                Funkcie.Output(bucket_weight, best_error, error, bucket_points, best_solution);
+            }
+            //vyber top 50 percent    
+            Funkcie.Top50(gen, solutions, solutions_50, k);
+
+            //CROSSOVER   
+            //mazanie pamate
+            Funkcie.MemErase(pamat);
+
+            // vytvarame pole stringov
+            Funkcie.StringConvert(gen, k, solutions_50, spojeny_solutions);
+
+            //JEDNOBODOVE krizenie
+            if (kriz == 1) {
+                for (int i = 0; i < gen / 4; i++) {
+                    //generovanie nahodneho cisla na krizenie
+                    int rnd = (int) (Math.random() * (k - 1) + 1);
+
+                    //vyber prvej dvojice a krizenie
+                    if (i == 0) {
+                        Funkcie.FirstPairChoose(gen, pamat);
+                        Funkcie.SinglePointCross(gen, rnd, k, pamat, spojeny_solutions, final_solutions, i);
+                    } //vyber ostavajucich dvojic a krizenie
+                    else {
+                        Funkcie.OtherPairChoose(gen, pamat, i, rnd, k, spojeny_solutions, final_solutions);
+                        Funkcie.SinglePointCross(gen, rnd, k, pamat, spojeny_solutions, final_solutions, i);
+                    }
+                }
+            }
+            //DVOJBODOVE krizenie
+            if (kriz == 2) {
+                for (int i = 0; i < gen / 4; i++) {
+                    //generovanie nahodnych cisel na krizenie       
+                    int rnd = (int) (Math.random() * (k - 2) + 1);
+                    int rnd2kriz = (int) (Math.random() * (k - 1) + 1);
+
+                    while (rnd2kriz <= rnd) {
+                        rnd2kriz = (int) (Math.random() * (k - 1) + 1);
+                    }
+                    //vyber prvej dvojice a krizenie
+                    if (i == 0) {
+                        Funkcie.FirstPairChoose(gen, pamat);
+                        Funkcie.TwoPointCross(gen, rnd, k, pamat, spojeny_solutions, final_solutions, i, rnd2kriz);
+                    } //vyber ostavajucich dvojic a krizenie
+                    else {
+                        Funkcie.OtherPairChoose(gen, pamat, i, rnd, k, spojeny_solutions, final_solutions);
+                        Funkcie.TwoPointCross(gen, rnd, k, pamat, spojeny_solutions, final_solutions, i, rnd2kriz);
+                    }
+                }
+            }
+            //pridanie povodnych jedincov k potomkom
+            System.arraycopy(spojeny_solutions, 0, final_solutions, gen / 2, gen / 2);
+
+            //prevod stringu na char kvoli mutacii
+            String string = "";
+            for (String i : final_solutions) {
+                string = string + i;
+            }
+            char[] final_solutions_char = string.toCharArray();
+
+            //MUTACIA
+            Funkcie.Mutation(gen, k, rmut, prah, final_solutions_char);
+
+            //prevod char naspat na string
+            String final_solutions_str = String.copyValueOf(final_solutions_char);
+
+            //rozdelenie stringu na vedra
+            Funkcie.StrtoBucket(gen, k, final_solutions_strings, final_solutions_str);
+
+            //prevod z final_solutions_char [0,1,1,0,1,...] na solutions[[01101][11001]...]
+            Funkcie.ChartoSolutions(gen, k, final_solutions_char, solutions);
+
+            //vypocet vah a bodov      
+            Funkcie.CountValues(gen, bucket_weight, bucket_points, k, final_solutions_char, weights, points);
+
+            //pocitanie chyby
+            Funkcie.CountError(gen, max_b, bucket_weight, error);
+
+            //triedenie
+            Funkcie.Sort(gen, error, temp_arr, bucket_weight, bucket_points, solutions);
+
+            //najdenie najlepsieho
+            //Funkcie.FindBest(error, best_error, k, best_solution, solutions, best_points, best_weight, bucket_points, bucket_weight);
+            if (error[0] < best_error) {
+                System.arraycopy(solutions[0], 0, best_solution, 0, k);
+
+                best_error = error[0];
+                best_points = bucket_points[0];
+                best_weight = bucket_weight[0];
+            }
+            Funkcie.Output(bucket_weight, best_error, error, bucket_points, best_solution);
+        }
+        Funkcie.OutputCrossNumber(kriz);
+    }
+    
+}
---
+++ DiplomovaPraca/src/diplomovapraca/Funkcie.java
@@ -0,0 +1,216 @@
+package diplomovapraca;
+//@author Peter Micek
+
+import java.util.Arrays;
+
+public class Funkcie {
+
+    public static void GenerateItems(int k, int max_w, int min_w, int max_p, int min_p, int[] weights, int[] points) {
+        for (int i = 0; i < k; i++) {
+
+            int x = (int) (Math.random() * (max_w - min_w + 1) + min_w);
+            weights[i] = x;
+
+            int y = (int) (Math.random() * (max_p - min_p + 1) + min_p);
+            points[i] = y;
+        }
+    }
+
+    public static void GenerateBuckets(int gen, int k, int[] bucket, int[] bucket_weight, int[] weights, int[] bucket_points, int[] points, int[][] solutions) {
+        for (int j = 0; j < gen; j++) {
+            for (int i = 0; i < k; i++) {
+
+                int z = (int) (Math.random() * 2);
+                bucket[i] = z;
+
+                if (z == 1) {
+                    bucket_weight[j] = (int) (bucket_weight[j] + weights[i]);
+                    bucket_points[j] = (int) (bucket_points[j] + points[i]);
+                }
+                solutions[j][i] = bucket[i];
+            }
+        }
+    }
+
+    public static void ErrorCount(int gen, int max_b, int[] error, int[] bucket_weight) {
+        for (int j = 0; j < gen; j++) {
+            error[j] = Math.abs(bucket_weight[j] - max_b);
+        }
+    }
+
+    public static void Sort(int gen, int[] error, int[] temp_arr, int[] bucket_weight, int[] bucket_points, int[][] solutions) {
+        for (int m = 0; m < gen; m++) {
+            for (int n = 1; n < (gen - m); n++) {
+                if (error[n - 1] > error[n]) {
+                    int temp = error[n - 1];
+                    error[n - 1] = error[n];
+                    error[n] = temp;
+
+                    temp_arr = solutions[n - 1];
+                    solutions[n - 1] = solutions[n];
+                    solutions[n] = temp_arr;
+
+                    temp = bucket_weight[n - 1];
+                    bucket_weight[n - 1] = bucket_weight[n];
+                    bucket_weight[n] = temp;
+
+                    temp = bucket_points[n - 1];
+                    bucket_points[n - 1] = bucket_points[n];
+                    bucket_points[n] = temp;
+                }
+            }
+        }
+    }
+
+    public static void Output(int[] bucket_weight, int best_error, int[] error, int[] bucket_points, int[] best_solution) {
+        System.out.println("najlepsie riesenie: " + Arrays.toString(best_solution));
+        System.out.println("vaha: " + bucket_weight[0]);
+        System.out.println("best_error: " + best_error);
+        System.out.println("chyba : " + error[0]);
+        System.out.println("skore : " + bucket_points[0]);
+    }
+
+    public static void Top50(int gen, int[][] solutions, int[][] solutions_50, int k) {
+        for (int i = 0; i < gen / 2; i++) {
+            System.arraycopy(solutions[i], 0, solutions_50[i], 0, k);
+        }
+    }
+
+    public static void MemErase(int[] pamat) {
+        for (int i = 0; i < pamat.length; i++) {
+            pamat[i] = 0;
+        }
+    }
+
+    public static void StringConvert(int gen, int k, int[][] solutions_50, String[] spojeny_solutions) {
+        for (int i = 0; i < (gen / 2); i++) {
+            for (int j = 0; j < k; j++) {
+                if (j == 0) {
+                    spojeny_solutions[i] = Integer.toString(solutions_50[i][j]);
+                } else {
+                    spojeny_solutions[i] = spojeny_solutions[i] + Integer.toString(solutions_50[i][j]);
+                }
+            }
+        }
+    }
+
+    public static void FirstPairChoose(int gen, int[] pamat) {
+        //vyber prvych dvojic
+        pamat[0] = (int) (Math.random() * (gen / 2) + 1);
+        pamat[1] = (int) (Math.random() * (gen / 2) + 1);
+
+        while (pamat[1] == pamat[0]) {
+            pamat[1] = (int) (Math.random() * (gen / 2) + 1);
+        }
+    }
+
+    public static void SinglePointCross(int gen, int rnd, int k, int[] pamat, String[] spojeny_solutions, String[] final_solutions, int i) {
+        //krizenie
+        final_solutions[2 * i] = spojeny_solutions[pamat[2 * i] - 1].substring(0, rnd) + spojeny_solutions[pamat[2 * i + 1] - 1].substring(rnd, k);
+        final_solutions[2 * i + 1] = spojeny_solutions[pamat[2 * i + 1] - 1].substring(0, rnd) + spojeny_solutions[pamat[2 * i] - 1].substring(rnd, k);
+    }
+
+    public static void OtherPairChoose(int gen, int[] pamat, int i, int rnd, int k, String[] spojeny_solutions, String[] final_solutions) {
+        //generovanie prveho z dvojice
+        int rnd1 = (int) (Math.random() * (gen / 2) + 1);
+        int opak1 = 1;
+
+        while (opak1 == 1) {
+            for (int j = 0; j < pamat.length; j++) {
+                if (rnd1 == pamat[j] || rnd1 == pamat[0]) {
+                    rnd1 = (int) (Math.random() * (gen / 2) + 1);
+                    j = 0;
+                } else {
+                    opak1 = 0;
+                }
+            }
+        }
+        pamat[2 * i] = rnd1;
+
+        //vyber druheho z dvojice
+        int rnd2 = (int) (Math.random() * (gen / 2) + 1);
+        int opak2 = 1;
+
+        while (opak2 == 1) {
+            for (int j = 0; j < pamat.length; j++) {
+                if (rnd2 == pamat[j] || rnd2 == pamat[0]) {
+                    rnd2 = (int) (Math.random() * (gen / 2) + 1);
+                    j = 0;
+                } else {
+                    opak2 = 0;
+                }
+            }
+        }
+        pamat[2 * i + 1] = rnd2;
+    }
+
+    public static void TwoPointCross(int gen, int rnd, int k, int[] pamat, String[] spojeny_solutions, String[] final_solutions, int i, int rnd2kriz) {
+        //krizenie
+        final_solutions[2 * i] = spojeny_solutions[pamat[2 * i] - 1].substring(0, rnd) + spojeny_solutions[pamat[2 * i + 1] - 1].substring(rnd, rnd2kriz) + spojeny_solutions[pamat[2 * i] - 1].substring(rnd2kriz, k);
+        final_solutions[2 * i + 1] = spojeny_solutions[pamat[2 * i + 1] - 1].substring(0, rnd) + spojeny_solutions[pamat[2 * i] - 1].substring(rnd, rnd2kriz) + spojeny_solutions[pamat[2 * i + 1] - 1].substring(rnd2kriz, k);
+    }
+
+    public static void Mutation(int gen, int k, int rmut, int prah, char[] final_solutions_char) {
+        for (int i = 0; i < gen * k; i++) {
+            int rndmut = (int) (Math.random() * (rmut + 1));
+            if (rndmut > prah) {
+                if (final_solutions_char[i] == '1') {
+                    final_solutions_char[i] = '0';
+                } else {
+                    final_solutions_char[i] = '1';
+                }
+            }
+        }
+    }
+
+    public static void StrtoBucket(int gen, int k, String[] final_solutions_strings, String final_solutions_str) {
+        for (int i = 0; i < gen; i++) {
+            final_solutions_strings[i] = final_solutions_str.substring(i * (k), i * (k) + (k));
+        }
+    }
+
+    public static void ChartoSolutions(int gen, int k, char[] final_solutions_char, int[][] solutions) {
+        for (int i = 0; i < gen; i++) {
+            for (int j = 0; j < k; j++) {
+                solutions[i][j] = Character.getNumericValue(final_solutions_char[i * k + j]);
+            }
+        }
+    }
+
+    public static void CountValues(int gen, int[] bucket_weight, int[] bucket_points, int k, char[] final_solutions_char, int[] weights, int[] points) {
+        for (int i = 0; i < gen; i++) {
+            bucket_weight[i] = 0;
+            bucket_points[i] = 0;
+
+            for (int j = 0; j < k; j++) {
+                if (final_solutions_char[i * k + j] == '1') {
+                    bucket_weight[i] = (int) (bucket_weight[i] + weights[j]);
+                    bucket_points[i] = (int) (bucket_points[i] + points[j]);
+                }
+            }
+        }
+    }
+
+    public static void CountError(int gen, int max_b, int[] bucket_weight, int[] error) {
+        for (int j = 0; j < gen; j++) {
+            error[j] = Math.abs(bucket_weight[j] - max_b);
+        }
+    }
+
+//    public static void FindBest(int[] error, int best_error, int k, int [] best_solution, int [][] solutions, int best_points, int best_weight, int[] bucket_points, int[] bucket_weight){
+//        if (error[0] < best_error) {
+//            System.arraycopy(solutions[0], 0, best_solution, 0, k);
+//
+//            best_error = error[0];
+//            best_points = bucket_points[0];
+//            best_weight = bucket_weight[0];
+//        }
+//    }
+    public static void OutputCrossNumber(int kriz) {
+        if (kriz == 1) {
+            System.out.println("zvolene " + kriz + "-bodove krizenie");
+        } else {
+            System.out.println("zvolene " + kriz + "-bodove krizenie");
+        }
+    }
+}
